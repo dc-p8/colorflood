3fc4a5a7d133c016f1f0f945dfa3d69829a37bf1
dc-p8 <cardinal.dorian@gmail.com>
2017-12-06 17:02:59 +0100

    Easier to understant play function but less optimized

--- app/src/main/java/com/example/dc/colorflood/LevelOnPlay.java
+++ app/src/main/java/com/example/dc/colorflood/LevelOnPlay.java
@@ -4,6 +4,7 @@ package com.example.dc.colorflood;
 import android.os.Bundle;
 import android.util.Pair;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.LinkedList;
 
@@ -83,41 +84,14 @@ class LevelOnPlay {
         this.currentLevel = currentLevel;
     }
 
-    void play(int newColor) {
-        this.updateMoves();
-        int nbCaseColored = 0;
-        LinkedList<Pair<Integer,Integer>> changeFifo = new LinkedList<>();
-        LinkedList<Pair<Integer,Integer>> winFifo = new LinkedList<>();
-        HashSet<Pair<Integer,Integer>> checked = new HashSet<>();
-        Pair<Integer,Integer> currentCase = new Pair<>(0,0);
-        changeFifo.add(currentCase);
-        checked.add(currentCase);
-        int oldColor = this.lvl.getCases()[0][0];
-        while(!changeFifo.isEmpty()){
-            nbCaseColored++;
-            currentCase = changeFifo.poll();
-            this.lvl.setCase(currentCase.first, currentCase.second, newColor);
-            for(int i = -1; i<2; i++){
-                for (int j = -1; j<2 ; j++){
-                    if(Math.abs(i)==Math.abs(j))
-                        continue;
-                    Pair<Integer,Integer> nextCase = new Pair<>(currentCase.first + i, currentCase.second + j);
-                    if (nextCase.first < this.lvl.getNbCasesWidth() && nextCase.first >= 0
-                            && nextCase.second < this.lvl.getNbCasesHeight() && nextCase.second >= 0) {
-                        updateFifo(checked, changeFifo, nextCase, oldColor);
-                        updateFifo(checked, winFifo, nextCase, newColor);
-                    }
-                }
-            }
-        }
-        if (hasWon(nbCaseColored, winFifo, newColor, checked)) {
-            this.win();
-            return;
-        }
-        if (this.hasLost())
-            this.lose();
-    }
-
+    /**
+     * Ajoute dans la queue fifo la case nextCase si elle n'a pas déjà été visitée et si elle est de la couleur color
+     * Met aussi à jour la liste checked
+     * @param checked
+     * La liste des cases déjà vérifiés
+     * @param fifo
+     * La queue à remplir
+     */
     private void updateFifo(HashSet<Pair<Integer,Integer>> checked, LinkedList<Pair<Integer,Integer>> fifo, Pair<Integer,Integer> nextCase, int color){
         if (this.lvl.getCases()[nextCase.first][nextCase.second] == color) {
             if (!checked.contains(nextCase)) {
@@ -125,6 +99,94 @@ class LevelOnPlay {
                 checked.add(nextCase);
             }
         }
+    }
+
+    /**
+     * Retourne la liste des cases connexes de même couleur
+     * @return
+     */
+    ArrayList<Pair<Integer, Integer>> getGroup()
+    {
+        ArrayList<Pair<Integer, Integer>> ret = new ArrayList<>(); // groupe à retourner
+
+        LinkedList<Pair<Integer,Integer>> changeFifo = new LinkedList<>(); // liste d'attente des cases à visiter
+        HashSet<Pair<Integer,Integer>> checked = new HashSet<>(); // liste des cases visités
+        Pair<Integer,Integer> currentCase = new Pair<>(0,0); // case actuelle
+        changeFifo.add(currentCase);
+        checked.add(currentCase);
+        int oldColor = this.lvl.getCases()[0][0];
+
+        while(!changeFifo.isEmpty()){
+            currentCase = changeFifo.poll();
+            ret.add(currentCase);
+            for(int i = -1; i<2; i++){
+                for (int j = -1; j<2 ; j++){
+                    if(Math.abs(i)==Math.abs(j)) // si la case est 0, 0 ou est une diagonale
+                        continue;
+                    Pair<Integer,Integer> nextCase = new Pair<>(currentCase.first + i, currentCase.second + j);
+                    if (nextCase.first < this.lvl.getNbCasesWidth() && nextCase.first >= 0
+                            && nextCase.second < this.lvl.getNbCasesHeight() && nextCase.second >= 0) {
+                        // on vérifie que la nouvelle case est dans le champ
+                        updateFifo(checked, changeFifo, nextCase, oldColor);
+                    }
+                }
+            }
+        }
+        return ret;
+    }
+
+    /**
+     * Condition de victoire
+     * @return
+     */
+    private boolean checkWin()
+    {
+        int color = this.lvl.getCases()[0][0];
+        for(int i = 0; i < this.lvl.getNbCasesWidth(); ++i)
+        {
+            for(int j = 0; j < this.lvl.getNbCasesHeight(); ++j)
+            {
+                if(this.lvl.getCases()[i][j] != color)
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Condition de défaite
+     * @return
+     */
+    private boolean checkLost() {
+        return this.nbMoves == this.lvl.getMaxNbMoves() + this.extraMoves;
+    }
+
+    /**
+     * Joue un coup en fonction de la nouvelle newColor
+     * @param newColor
+     */
+    void play(int newColor)
+    {
+        // on commence par actualiser le nombre de coups restant
+        this.updateMoves();
+
+        // on créer le groupe de cases connexes de même couleur
+        ArrayList<Pair<Integer,Integer>> group = getGroup();
+
+        // on change leur couleur
+        for (Pair<Integer,Integer> p : group) {
+            this.lvl.setCase(p.first, p.second, newColor);
+        }
+
+        // on vérifie si on a gagné
+        if (checkWin()) {
+            this.win();
+            return;
+        }
+        if (this.checkLost()) {
+            this.lose();
+        }
+
     }
 
     private void win(){
@@ -135,29 +197,9 @@ class LevelOnPlay {
         this.onLevelEventListener.onLose();
     }
 
-    private boolean hasWon(int nbCaseColored, LinkedList<Pair<Integer,Integer>> winFifo, int winColor, HashSet<Pair<Integer,Integer>> checked) {
-        while(!winFifo.isEmpty()){
-            nbCaseColored++;
-            Pair<Integer,Integer> currentCase = winFifo.poll();
-            for(int i = -1; i<2; i++) {
-                for (int j = -1; j < 2; j++) {
-                    if(Math.abs(i)==Math.abs(j))
-                        continue;
-                    Pair<Integer, Integer> nextCase = new Pair<>(currentCase.first + i, currentCase.second + j);
-                    if (nextCase.first < this.lvl.getNbCasesWidth() && nextCase.first >= 0
-                            && nextCase.second < this.lvl.getNbCasesHeight() && nextCase.second >= 0) {
-                        updateFifo(checked, winFifo, nextCase, winColor);
-                    }
-                }
-            }
-        }
-        return nbCaseColored == this.lvl.getNbCasesHeight() * this.lvl.getNbCasesWidth();
-    }
-
-    private boolean hasLost() {
-        return this.nbMoves == this.lvl.getMaxNbMoves() + this.extraMoves;
-    }
-
+    /**
+     * Met à jour les mouvements restants
+     */
     private void updateMoves(){
         if (this.nbMoves != this.lvl.getMaxNbMoves())
             this.nbMoves++;
@@ -165,12 +207,20 @@ class LevelOnPlay {
             this.onLevelEventListener.decExtraMoves();
     }
 
+    /**
+     * Initialise le niveau
+     * @param nbColors
+     * Nombre de couleurs du nieau
+     */
     void initLevel(int nbColors) {
         this.nbMoves = 0;
         this.lvl.init(nbColors);
         this.savedLvl = new Level(this.lvl);
     }
 
+    /**
+     * Redémmare le niveau en fonction de la sauvegarde
+     */
     void restart(){
         this.nbMoves = 0;
         this.lvl = new Level(this.savedLvl);
