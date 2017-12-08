81dce301c73e9ea96cb6870e57a5b19af4672e56
dc-p8 <cardinal.dorian@gmail.com>
2017-12-08 15:58:04 +0100

    Added action bar

--- app/src/main/java/com/example/dc/colorflood/Credits.java
+++ app/src/main/java/com/example/dc/colorflood/Credits.java
@@ -10,6 +10,14 @@ public class Credits extends MusicActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setContentView(R.layout.activity_credits);
     }
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return true;
+
+    }
+
 }
--- app/src/main/java/com/example/dc/colorflood/SystemMenu.java
+++ app/src/main/java/com/example/dc/colorflood/SystemMenu.java
@@ -16,8 +16,17 @@ public class SystemMenu extends MusicActivity {
     ToggleButton toggleAudio;
 
     @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return true;
+
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         gameViewModel = GameViewModel.getInstance();
         setContentView(R.layout.activity_system);
         final Context c = this;
